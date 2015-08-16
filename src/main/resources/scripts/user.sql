720e40062625550f84c3b8bec65bbac25e86b2cc
Sergey Bochkov <bochkovsa@gmail.com>
2015-08-16 12:43:01 +0500

    поправлены скрипты

--- src/main/resources/scripts/user.sql
+++ src/main/resources/scripts/user.sql
@@ -1,51 +1,25 @@
-# Дамп таблицы roles
-# ------------------------------------------------------------
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles(
+	id bigint NOT NULL,
+	role character varying(255),
+	CONSTRAINT roles_pkey PRIMARY KEY (id));
 
-DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+  id bigint NOT NULL,
+  password character varying(255),
+  username character varying(255),
+  CONSTRAINT users_pkey PRIMARY KEY (id));
 
-CREATE TABLE `roles` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `role` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `role` (`role`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-INSERT INTO `roles` (`id`, `role`)
-VALUES
-    (1,'ADMIN'),
-    (3,'GUEST'),
-    (2,'USER');
-
-# Дамп таблицы users
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(250) DEFAULT NULL,
-  `password` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `users` (`id`, `username`, `password`)
-VALUES
-    (1,'user','user');
-
-# Дамп таблицы users_roles
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users_roles`;
-
-CREATE TABLE `users_roles` (
-  `user_id` bigint(20) unsigned DEFAULT NULL,
-  `role_id` bigint(20) unsigned DEFAULT NULL,
-  KEY `hasuser` (`user_id`),
-  KEY `hasrole` (`role_id`),
-  CONSTRAINT `hasrole` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `hasuser` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS users_roles;
+CREATE TABLE users_roles(
+  user_id bigint NOT NULL,
+  role_id bigint NOT NULL,
+  CONSTRAINT users_roles_pkey PRIMARY KEY (user_id, role_id),
+  CONSTRAINT fk_1hjw31qvltj7v3wb5o31jsrd8 FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_k2mq1ee4ob6uw649wgaus1ate FOREIGN KEY (role_id)
+      REFERENCES roles (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE CASCADE
+)
